540332ace9cdae1f644d6d59f237272029e6f958
terroroir <81487235+terroroir@users.noreply.github.com>
2025-08-05 17:25:37 -0500

    ok

--- src/app.js
+++ src/app.js
@@ -192,7 +192,7 @@ if (invoiceForm.email) {
 
     await emailjs.send(
       '62ytl14', // 1. Your Service ID // 
-      'sp3icre', // 2. Your Template ID (get this from the EmailJS dashboard)
+      'template_w0q56ps', // 2. Your Template ID (get this from the EmailJS dashboard)
       templateParams, // 3. The template parameters object
       '8JO0XTTh5R62cNEbP', // 4. Your Public Key (User ID)
     );
